77fbf1db4fc2560922c4680186b87062f6c12b9a
vmarshirov <vmarshirov@outlook.com>
2024-04-04 19:10:44 +0300

    site_app

--- site_app/admin.py
+++ site_app/admin.py
@@ -6,7 +6,7 @@ from .models import *
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['id', 'item_title', 'item_nav', 'item_nav_position', 'item_content', 'item_current_date']
-    list_display_links = ['item_title', ]
+    list_display_links = ['id','item_title', ]
     list_editable = ['item_nav', 'item_nav_position',]
     search_fields = ['item_content',]
     list_filter = ['item_title', 'item_nav']
--- site_app/models.py
+++ site_app/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
 
     item_nav = models.CharField(verbose_name = 'Название ссылки', max_length=255, default="Название ссылки")
 
-    item_nav_position = models.IntegerField(verbose_name = 'Приоритет в навигации (0 - исключить)', default=1,)
+    item_nav_position = models.IntegerField(verbose_name = 'Приоритет ссылки в навигации (0 - исключить)', default=1, help_text="большее - число правее, 0 - исключить из навигации")
 
     item_content = models.TextField(verbose_name = 'Основное содержание страницы', 
     default = '<a id="top_of _page" href="#h1" class="card-link">На раздел 1</a>   <h6 class="card-subtitle mb-2 text-body-secondary ">   <a id="h1">Раздел 1</a></h6>   <p class="card-text">Содержание раздела 1</p>   <img src="/static/images/settings_django.png"  height="30px">    <a href="#top_of _page" class="card-link">к началу страницы</a>')
@@ -15,10 +15,10 @@ class Item(models.Model):
     item_current_date = models.DateTimeField(verbose_name = "Дата Записи", auto_now=True)
 
     class Meta:
-        verbose_name = 'Контент текущей страницы'
-        verbose_name_plural = 'Уникальный контент страниц'
+        verbose_name = 'Содержание текущей страницы'
+        verbose_name_plural = 'Содержание всех страниц'
         ordering = ('-item_nav_position',)
 
     def __str__(self):
-        return f"id: {self.id}.  {self.item_title}"
+        return f"pk: {self.pk};  item_title: {self.item_title}"
 
